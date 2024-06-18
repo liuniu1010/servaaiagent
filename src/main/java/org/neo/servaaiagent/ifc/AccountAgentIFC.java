2bd0a983be866219b21a0f42aa118d1c950ab91b
Niu Liu <liuniu@tsinghua.org.cn>
2024-06-18 12:49:23 +1200

    init AccountAgent

---
+++ src/main/java/org/neo/servaaiagent/ifc/AccountAgentIFC.java
@@ -0,0 +1,23 @@
+package org.neo.servaaiagent.ifc;
+
+import org.neo.servaframe.interfaces.DBConnectionIFC;
+
+public interface AccountAgentIFC {
+    public void sendPassword(String username);
+    public void sendPassword(DBConnectionIFC dbConnection, String username);
+
+    public String login(String username, String password);
+    public String login(DBConnectionIFC dbConnection, String username, String password);
+
+    public boolean checkLogin(String session);
+    public boolean checkLogin(DBConnectionIFC dbConnection, String session);
+
+    public void purchaseCredits(String session, int credits);
+    public void purchaseCredits(DBConnectionIFC dbConnection, String session, int credits);
+
+    public void consumeCredits(String session, int credits);
+    public void consumeCredits(DBConnectionIFC dbConnetion, String session, int credits);
+
+    public boolean checkCredits(String session);
+    public boolean checkCredits(DBConnectionIFC dbConnection, String session);
+}
---
+++ src/main/java/org/neo/servaaiagent/impl/AccountAgentImpl.java
@@ -0,0 +1,124 @@
+package org.neo.servaaiagent.impl;
+
+import org.neo.servaframe.interfaces.DBConnectionIFC;
+import org.neo.servaframe.interfaces.DBServiceIFC;
+import org.neo.servaframe.interfaces.DBSaveTaskIFC;
+import org.neo.servaframe.ServiceFactory;
+
+import org.neo.servaaiagent.ifc.AccountAgentIFC;
+
+public class AccountAgentImpl implements AccountAgentIFC, DBSaveTaskIFC {
+    private AccountAgentImpl() {
+    }
+
+    public static AccountAgentImpl getInstance() {
+        return new AccountAgentImpl();
+    }
+
+    @Override
+    public Object save(DBConnectionIFC dbConnection) {
+        return null;
+    }
+
+    @Override
+    public void sendPassword(String username) {
+        // no input dbConnection, start/commmit transaction itself
+        DBServiceIFC dbService = ServiceFactory.getDBService();
+        dbService.executeSaveTask(new AccountAgentImpl() {
+            @Override
+            public Object save(DBConnectionIFC dbConnection) {
+                sendPassword(dbConnection, username);
+                return null;
+            }
+        });
+    }
+
+    @Override
+    public void sendPassword(DBConnectionIFC dbConnection, String username) {
+    }
+
+    @Override
+    public String login(String username, String password) {
+        // no input dbConnection, start/commmit transaction itself
+        DBServiceIFC dbService = ServiceFactory.getDBService();
+        return (String)dbService.executeSaveTask(new AccountAgentImpl() {
+            @Override
+            public Object save(DBConnectionIFC dbConnection) {
+                return login(dbConnection, username, password);
+            }
+        });
+    }
+
+    @Override
+    public String login(DBConnectionIFC dbConnection, String username, String password) {
+        return null;
+    }
+
+    @Override
+    public boolean checkLogin(String session) {
+        // no input dbConnection, start/commmit transaction itself
+        DBServiceIFC dbService = ServiceFactory.getDBService();
+        return (boolean)dbService.executeSaveTask(new AccountAgentImpl() {
+            @Override
+            public Object save(DBConnectionIFC dbConnection) {
+                return checkLogin(dbConnection, session);
+            }
+        });
+    }
+
+    @Override
+    public boolean checkLogin(DBConnectionIFC dbConnection, String session) {
+        return false;
+    }
+
+    @Override
+    public void purchaseCredits(String session, int credits) {
+        // no input dbConnection, start/commmit transaction itself
+        DBServiceIFC dbService = ServiceFactory.getDBService();
+        dbService.executeSaveTask(new AccountAgentImpl() {
+            @Override
+            public Object save(DBConnectionIFC dbConnection) {
+                purchaseCredits(dbConnection, session, credits);
+                return null;
+            }
+        });
+    }
+
+    @Override
+    public void purchaseCredits(DBConnectionIFC dbConnection, String session, int credits) {
+    }
+
+    @Override
+    public void consumeCredits(String session, int credits) {
+        // no input dbConnection, start/commmit transaction itself
+        DBServiceIFC dbService = ServiceFactory.getDBService();
+        dbService.executeSaveTask(new AccountAgentImpl() {
+            @Override
+            public Object save(DBConnectionIFC dbConnection) {
+                consumeCredits(dbConnection, session, credits);
+                return null;
+            }
+        });
+    }
+
+    @Override
+    public void consumeCredits(DBConnectionIFC dbConnetion, String session, int credits) {
+    }
+
+    @Override
+    public boolean checkCredits(String session) {
+        // no input dbConnection, start/commmit transaction itself
+        DBServiceIFC dbService = ServiceFactory.getDBService();
+        return (boolean)dbService.executeSaveTask(new AccountAgentImpl() {
+            @Override
+            public Object save(DBConnectionIFC dbConnection) {
+                return checkCredits(dbConnection, session);
+            }
+        });
+    }
+
+    @Override
+    public boolean checkCredits(DBConnectionIFC dbConnection, String session) {
+        return false;
+    }
+}
