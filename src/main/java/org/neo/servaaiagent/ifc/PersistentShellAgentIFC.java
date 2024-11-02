7d07f98ef12995ba2ace4593a5cdc7b2101f7da4
Niu Liu <liuniu@tsinghua.org.cn>
2024-11-03 10:48:20 +1300

    some basic implementation on PersistentShell Agent

--- src/main/java/org/neo/servaaiagent/ifc/PersistentShellAgentIFC.java
+++ src/main/java/org/neo/servaaiagent/ifc/PersistentShellAgentIFC.java
@@ -3,8 +3,8 @@ package org.neo.servaaiagent.ifc;
 import org.neo.servaframe.interfaces.DBConnectionIFC;
 
 public interface PersistentShellAgentIFC {
-    public String execute(String session, String command);
-    public String execute(DBConnectionIFC dbConnection, String session, String command);
+    public String execute(String session, String input);
+    public String execute(DBConnectionIFC dbConnection, String session, String input);
 
     public void terminateShell(String session);
     public void terminateShell(DBConnectionIFC dbConnection, String session);
--- src/main/java/org/neo/servaaiagent/impl/PersistentShellAgentImpl.java
+++ src/main/java/org/neo/servaaiagent/impl/PersistentShellAgentImpl.java
@@ -1,19 +1,37 @@
 package org.neo.servaaiagent.ifc;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.neo.servaframe.interfaces.DBConnectionIFC;
+import org.neo.servaaibase.util.CommonUtil;
+import org.neo.servaaibase.NeoAIException;
 import org.neo.servaaiagent.ifc.PersistentShellAgentIFC;
 
 public class PersistentShellAgentImpl implements PersistentShellAgentIFC {
+    Map<String, PersistentShell> shellCache = new ConcurrentHashMap<String, PersistentShell>();
+
     @Override
-    public String execute(String session, String command) {
-        // to be implemented
-        return null;
+    public String execute(String session, String input) {
+        try {
+            PersistentShell shell = getShell(session);
+            return shell.executeCommand(input);
+        }
+        catch(NeoAIException nex) {
+            throw nex;
+        }
+        catch(Exception ex) {
+            throw new NeoAIException(ex.getMessage(), ex);
+        }
     }
 
     @Override
-    public String execute(DBConnectionIFC dbConnection, String session, String command) {
+    public String execute(DBConnectionIFC dbConnection, String session, String input) {
         // to be implemented
         return null;
     }
@@ -23,8 +41,19 @@ public class PersistentShellAgentImpl implements PersistentShellAgentIFC {
         // to be implemented
     }
 
+    @Override 
     public void terminateShell(DBConnectionIFC dbConnection, String session) {
         // to be implemented
+    }
+
+    private PersistentShell getShell(String session) throws Exception {
+        if(shellCache.containsKey(session)) {
+            return shellCache.get(session);
+        }
+
+        PersistentShell shell = new PersistentShell();
+        shellCache.put(session, shell);
+        return shell;
     }
 }
 
@@ -32,10 +61,9 @@ class PersistentShell {
     private Process shellProcess;
     private BufferedWriter shellWriter;
     private BufferedReader shellReader;
-    private BufferedReader shellErrorReader;
 
     public PersistentShell() throws IOException {
-        ProcessBuilder pb = new ProcessBuilder("/bin/bash");
+        ProcessBuilder pb = new ProcessBuilder(new String[]{"/bin/sh", "-c", ""});
         pb.redirectErrorStream(true);
         shellProcess = pb.start();
 
@@ -43,12 +71,12 @@ class PersistentShell {
         shellReader = new BufferedReader(new InputStreamReader(shellProcess.getInputStream()));
     }
 
-    public String executeCommand(String command) throws IOException {
-        shellWriter.write(command);
+    public String executeCommand(String input) throws IOException {
+        shellWriter.write(input);
         shellWriter.newLine();
         shellWriter.flush();
 
-        // Add a unique marker to identify when the command output ends
+        // Add a unique marker to identify when the input output ends
         String marker = "END_OF_COMMAND_OUTPUT_" + System.currentTimeMillis();
         shellWriter.write("echo " + marker);
         shellWriter.newLine();
