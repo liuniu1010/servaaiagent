a719646f6449a80dccfd12013eca70284a7169d9
Niu Liu <liuniu@tsinghua.org.cn>
2025-06-26 15:48:36 +1200

    add skeleton of two extra methods in AssistantCallImpl

--- src/main/java/org/neo/servaaiagent/impl/AssistantCallImpl.java
+++ src/main/java/org/neo/servaaiagent/impl/AssistantCallImpl.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import org.neo.servaframe.interfaces.DBConnectionIFC;
 import org.neo.servaframe.interfaces.DBServiceIFC;
 import org.neo.servaframe.interfaces.DBQueryTaskIFC;
-import org.neo.servaframe.interfaces.DBSaveTaskIFC;
 import org.neo.servaframe.ServiceFactory;
 import org.neo.servaframe.model.SQLStruct;
 
@@ -41,9 +40,13 @@ public class AssistantCallImpl implements FunctionCallIFC {
 
     private static List<AIModel.Function> innerGetFunctions() {
         AIModel.Function getCreditsLeft = generateFunctionForGetCreditsLeft();
+        AIModel.Function getCreditsTopupHistory = generateFunctionForGetCreditsTopupHistory();
+        AIModel.Function getCreditsConsumedHistory = generateFunctionForGetCreditsConsumedHistory();
 
         List<AIModel.Function> functions = new ArrayList<AIModel.Function>();
         functions.add(getCreditsLeft);
+        // functions.add(getCreditsTopupHistory);
+        // functions.add(getCreditsConsumedHistory);
 
         return functions;
     }
@@ -52,6 +55,12 @@ public class AssistantCallImpl implements FunctionCallIFC {
     public Object callFunction(AIModel.Call call) {
         if(call.getMethodName().equals(METHODNAME_GETCREDITSLEFT)) {
             return call_getCreditsLeft(call);
+        }
+        else if(call.getMethodName().equals(METHODNAME_GETCREDITSTOPUPHISTORY)) {
+            return call_getCreditsTopupHistory(call);
+        }
+        else if(call.getMethodName().equals(METHODNAME_GETCREDITSCONSUMEDHISTORY)) {
+            return call_getCreditsConsumedHistory(call);
         }
 
         return null;
@@ -79,6 +88,28 @@ public class AssistantCallImpl implements FunctionCallIFC {
         return function;
     }
 
+    private static AIModel.Function generateFunctionForGetCreditsTopupHistory() {
+        List<AIModel.FunctionParam> params = new ArrayList<AIModel.FunctionParam>();
+
+        AIModel.Function function = new AIModel.Function();
+        function.setMethodName(METHODNAME_GETCREDITSTOPUPHISTORY);
+        function.setParams(params);
+        function.setDescription("get credits topup history of the current user");
+
+        return function;
+    }
+
+    private static AIModel.Function generateFunctionForGetCreditsConsumedHistory() {
+        List<AIModel.FunctionParam> params = new ArrayList<AIModel.FunctionParam>();
+
+        AIModel.Function function = new AIModel.Function();
+        function.setMethodName(METHODNAME_GETCREDITSCONSUMEDHISTORY);
+        function.setParams(params);
+        function.setDescription("get credits consumed history of the current user");
+
+        return function;
+    }
+
     private static String METHODNAME_GETCREDITSLEFT = "getCreditsLeft";
     private String getCreditsLeft() {
         AccountAgentIFC accountAgent = AccountAgentImpl.getInstance();
@@ -87,7 +118,71 @@ public class AssistantCallImpl implements FunctionCallIFC {
         return result;
     }
 
+    private static String METHODNAME_GETCREDITSTOPUPHISTORY = "getCreditsTopupHistory";
+    private String getCreditsTopupHistory() {
+        try {
+            DBServiceIFC dbService = ServiceFactory.getDBService();
+            return (String)dbService.executeQueryTask(new DBQueryTaskIFC() {
+                @Override
+                public Object query(DBConnectionIFC dbConnection) {
+                    try {
+                        return getCreditsTopupHistory(dbConnection);
+                    }
+                    catch(RuntimeException rex) {
+                        throw rex;
+                    }
+                    catch(Exception ex) {
+                        throw new RuntimeException(ex.getMessage(), ex);
+                    }
+                }
+            });
+        }
+        catch(Exception ex) {
+            return "meet exception in getting topup history, the exception message is: " + ex.getMessage();
+        }
+    }
+
+    private String getCreditsTopupHistory(DBConnectionIFC dbConnection) throws Exception {
+        return "";
+    }
+
+    private static String METHODNAME_GETCREDITSCONSUMEDHISTORY = "getCreditsConsumedHistory";
+    private String getCreditsConsumedHistory() {
+        try {
+            DBServiceIFC dbService = ServiceFactory.getDBService();
+            return (String)dbService.executeQueryTask(new DBQueryTaskIFC() {
+                @Override
+                public Object query(DBConnectionIFC dbConnection) {
+                    try {
+                        return getCreditsConsumedHistory(dbConnection);
+                    }
+                    catch(RuntimeException rex) {
+                        throw rex;
+                    }
+                    catch(Exception ex) {
+                        throw new RuntimeException(ex.getMessage(), ex);
+                    }
+                }
+            });
+        }
+        catch(Exception ex) {
+            return "meet exception in getting topup history, the exception message is: " + ex.getMessage();
+        }
+    }
+
+    private String getCreditsConsumedHistory(DBConnectionIFC dbConnection) {
+        return null;
+    }
+
     private String call_getCreditsLeft(AIModel.Call call) {
         return getCreditsLeft();
+    }
+
+    private String call_getCreditsTopupHistory(AIModel.Call call) {
+        return getCreditsTopupHistory();
+    }
+
+    private String call_getCreditsConsumedHistory(AIModel.Call call) {
+        return getCreditsConsumedHistory();
     }
 }
